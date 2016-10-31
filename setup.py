c9b0afd95d3ad8f7f907da7e11ccb324392091ad
Momme Butenschoen <momm@pml.ac.uk>
2016-10-31 12:45:28 +0000

    Correction of setup metadata.

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     # https://packaging.python.org/en/latest/single_source_version.html
     version='16.11',
 
-    description='Python processing for scientific bibliographies.',
+    description='Python processing of netcdf files.',
     long_description=long_description,
 
     # The project's main homepage.
