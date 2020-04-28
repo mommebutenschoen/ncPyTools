748db3cd72601dba75941441559dfbb2fa89cf24
Momme Butenschön <momme.butenschon@cmcc.it>
2020-04-28 21:34:58 +0200

    Release 20.04.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='16.11',
+    version='20.04',
 
     description='Python processing of netcdf files.',
     long_description=long_description,
